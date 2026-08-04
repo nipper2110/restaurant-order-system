104a358043b16c8dffe71ac47106b5edeefb3e31
Chan Nyein Moe <channyeinmoe2121@gmail.com>
2026-08-04 09:29:59 +0700

    Add Forget Password

--- restaurant-backend/src/controllers/authController.ts
+++ restaurant-backend/src/controllers/authController.ts
@@ -345,7 +345,7 @@ export const login = [
         }
       }
       // Ending
-      return next(createError(req.t("wrongPassword"), 401, errorCode.invalid));
+      return next(createError("Password is wrong.", 401, errorCode.invalid));
     }
 
     // Authorization token
@@ -474,3 +474,249 @@ export const logout = async (
 
   res.status(200).json({ message: "Successfully logged out. See you soon." });
 };
+
+export const forgetPassword = [
+  body("email", "Invalid email").trim().notEmpty().isEmail().normalizeEmail(),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    if (errors.length > 0) {
+      return next(createError(errors[0].msg, 400, errorCode.invalid));
+    }
+
+    const email = req.body.email;
+
+    const user = await getUserByEmail(email);
+    checkUserIfNotExist(user);
+
+    const otp = 123456;
+    // const otp = generateOtp();
+    const salt = await bcrypt.genSalt(10);
+    const hashOtp = await bcrypt.hash(otp.toString(), salt);
+
+    const token = generateToken();
+
+    const otpRow = await getOtpByEmail(email);
+
+    let result;
+    const lastOtpRequest = new Date(otpRow!.updatedAt).toLocaleDateString();
+    const today = new Date().toLocaleDateString();
+    const isSameDate = lastOtpRequest === today;
+    checkOtpErrorIfSameDate(isSameDate, otpRow!.error);
+
+    if (!isSameDate) {
+      const otpData = {
+        otp: hashOtp,
+        rememberToken: token,
+        count: 1,
+        error: 0,
+      };
+      result = await updateOtp(otpRow!.id, otpData);
+    } else {
+      if (otpRow!.count === 3) {
+        return next(
+          createError(
+            "OTP is allowed to request 3 times per day.",
+            405,
+            errorCode.overLimit,
+          ),
+        );
+      } else {
+        const otpData = {
+          otp: hashOtp,
+          rememberToken: token,
+          count: otpRow!.count + 1,
+        };
+        result = await updateOtp(otpRow!.id, otpData);
+      }
+    }
+
+    res.status(200).json({
+      message: `We are sending OTP to ${result.email} to reset password.`,
+      email: result.email,
+      rememberToken: result.rememberToken,
+    });
+  },
+];
+
+export const verifyOtpForPassword = [
+  body("email", "Invalid email").trim().notEmpty().isEmail().normalizeEmail(),
+  body("otp", "Invalid OTP")
+    .trim()
+    .notEmpty()
+    .matches("^[0-9]+$")
+    .isLength({ min: 6, max: 6 }),
+  body("token", "Invalid token").trim().notEmpty().escape(),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    if (errors.length > 0) {
+      return next(createError(errors[0].msg, 400, errorCode.invalid));
+    }
+
+    const { email, otp, token } = req.body;
+
+    const user = await getUserByEmail(email);
+    checkUserIfNotExist(user);
+
+    const otpRow = await getOtpByEmail(email);
+
+    const lastOtpVerify = new Date(otpRow!.updatedAt).toLocaleDateString();
+    const today = new Date().toLocaleDateString();
+    const isSameDate = lastOtpVerify === today;
+    checkOtpErrorIfSameDate(isSameDate, otpRow!.error);
+
+    // Token is wrong
+    if (otpRow?.rememberToken !== token) {
+      const otpData = {
+        error: 5,
+      };
+      await updateOtp(otpRow!.id, otpData);
+
+      return next(createError("Invalid Token", 401, errorCode.invalid));
+    }
+
+    // OTP is expired
+    const isExpired = moment().diff(otpRow?.updatedAt, "minutes") > 2;
+    if (isExpired) {
+      return next(createError("OTP is expired", 403, errorCode.otpExpired));
+    }
+
+    const isMatchOTP = await bcrypt.compare(otp, otpRow!.otp);
+    // OTP is wrong
+    if (!isMatchOTP) {
+      // If OTP error is first time today
+      if (!isSameDate) {
+        const otpData = {
+          error: 1,
+        };
+        await updateOtp(otpRow!.id, otpData);
+      } else {
+        //If OTP error is not first time today
+        const otpData = {
+          error: {
+            increment: 1,
+          },
+        };
+        await updateOtp(otpRow!.id, otpData);
+      }
+
+      return next(createError("OTP is incorrect", 401, errorCode.invalid));
+    }
+
+    // All are OK
+    const verifyToken = generateToken();
+    const otpData = {
+      verifyToken,
+      error: 0,
+      count: 1,
+    };
+
+    const result = await updateOtp(otpRow!.id, otpData);
+
+    res.status(200).json({
+      message: "OTP is successfully verified to reset password.",
+      email: result.email,
+      token: verifyToken,
+    });
+  },
+];
+
+export const resetPassword = [
+  body("email", "Invalid email").trim().notEmpty().isEmail().normalizeEmail(),
+  body("password", "Password must be 6 digits")
+    .notEmpty()
+    .trim()
+    .matches("^[0-9]+$")
+    .isLength({ min: 6, max: 6 }),
+  body("token", "Invalid Token").notEmpty().trim().escape(),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    if (errors.length > 0) {
+      return next(createError(errors[0].msg, 400, errorCode.invalid));
+    }
+
+    const { email, password, token } = req.body;
+
+    const user = await getUserByEmail(email);
+    checkUserIfNotExist(user);
+
+    const otpRow = await getOtpByEmail(email);
+
+    // OTP error count is over limit
+    if (otpRow!.error === 5) {
+      return next(
+        createError("This request may be an attack", 400, errorCode.attack),
+      );
+    }
+
+    // Token is wrong
+    if (otpRow!.verifyToken !== token) {
+      const otpData = {
+        error: 5,
+      };
+      await updateOtp(otpRow!.id, otpData);
+      return next(createError("Invalid Token", 401, errorCode.invalid));
+    }
+
+    // Request is expired
+    const isExpired = moment().diff(otpRow!.updatedAt, "minutes") > 10;
+    if (isExpired) {
+      return next(
+        createError(
+          "Your request is expired. Please try again.",
+          403,
+          errorCode.requestExpired,
+        ),
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const accessTokenPayload = { id: user!.id };
+    const refreshTokenPayload = { id: user!.id, email: user!.email };
+
+    const accessToken = jwt.sign(
+      accessTokenPayload,
+      process.env.ACCESS_TOKEN_SECRET!,
+      {
+        expiresIn: 60 * 15,
+      },
+    );
+
+    const refreshToken = jwt.sign(
+      refreshTokenPayload,
+      process.env.REFRESH_TOKEN_SECRET!,
+      {
+        expiresIn: "30d",
+      },
+    );
+
+    // Updating randToken with refreshToken
+    const userUpdateData = {
+      password: hashPassword,
+      randToken: refreshToken,
+    };
+    await updateUser(user!.id, userUpdateData);
+
+    res
+      .cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 15 * 60 * 1000, // 15 min
+        path: "/",
+      })
+      .cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        path: "/",
+      })
+      .status(200)
+      .json({
+        message: "Successfully reset your password.",
+        userId: user!.id,
+      });
+  },
+];
--- restaurant-backend/src/routes/v1/auth.ts
+++ restaurant-backend/src/routes/v1/auth.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import {
   confirmPassword,
+  forgetPassword,
   login,
   logout,
   register,
+  resetPassword,
   verifyOtp,
+  verifyOtpForPassword,
 } from "../../controllers/authController";
 
 const router = express.Router();
@@ -14,6 +17,10 @@ router.post("/verify-otp", verifyOtp);
 router.post("/confirm-password", confirmPassword);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.post("/forget-password", forgetPassword);
+router.post("/verify", verifyOtpForPassword);
+router.post("/reset-password", resetPassword);
 
 // router.post("/change-password");
 
